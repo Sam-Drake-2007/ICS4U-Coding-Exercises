2c6f95aa4e35beae400084eaf39daaa67b514e0f
Samuel Drake <samueljhdrake@gmail.com>
2025-01-28 12:31:18 -0500

    Add files via upload

---
+++ U1 L1 Ex 3 Révision input et print/app.py
@@ -0,0 +1,6 @@
+nom = input("Entrer votre nom: ")
+age = input("Entrer votre age: ")
+ecole = input("Entrer le nom de votre école: ")
+nourriture = input("Entrer votre nourriture préférée: ")
+
+print(f"Ton nom est {nom}, tu as {age} ans, tu va à l'école {ecole} et ta nourriture préférée est: {nourriture}.")
